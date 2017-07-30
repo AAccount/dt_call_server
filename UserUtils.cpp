0484dd2363be3e9bac9741ebed37305b9b3f292b
Daniel <danieltjandra@gmail.com>
2017-07-30 16:54:07 -0400

    Remove redundant lines in clear session.

--- UserUtils.cpp
+++ UserUtils.cpp
@@ -185,8 +185,6 @@ void UserUtils::clearSession(std::string username)
 		//remove udp info
 		clearUdpInfo(username);
 
-		user->setSessionkey("");
-		user->setUserState(NONE);
 		removeCallPair(username);
 		//don't reset the challenge because when old fds exist when doing login1
 		//	the challenge that is set will be erased at the end of that select round.
